67bda27882a07a125245b791a1e441f87fa7ffcf
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-02 03:55:51 +0200

    Print numbers in two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,6 @@
 
 for num in range(100):
     if num != 99 :
-        if num < 10:
-            print("0{}, ".format(num), end="")
-        else:
-            print("{}, ".format(num), end="")
+        print("{:02d}".format(num), end=", ")
     else:
         print("{}".format(num))
