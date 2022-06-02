2396ae0dfee8c73c070ed541bd1e2026d1449f64
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-02 18:26:48 +0200

    Print command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    
+    args = sys.argv
+    len_args = len(args) - 1
+    if len_args == 0:
+        print("{} arguments.".format(len_args))
+    elif len_args == 1:
+        print("{} argument:".format(len_args))
+    else:
+        print("{} arguments:".format(len_args))
+    for index in range(1, len_args + 1):
+        print("{}: {}".format(index, args[index]))
