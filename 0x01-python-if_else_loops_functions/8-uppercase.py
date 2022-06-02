63b7f67e2a7c0bedeafd877cac9f9561295acf7d
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-02 04:40:18 +0200

    Change lower to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    str1 = ""
+    for c in str:
+        if ord(c) in range(ord('a'), ord('z') + 1):
+            str1 += chr(ord(c) - 32)
+        else:
+            str1 += c
+    print(str1)
