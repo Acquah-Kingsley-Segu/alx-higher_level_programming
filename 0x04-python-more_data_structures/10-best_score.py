af15e0586817489afd9c689d65d6a276d4c9052a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-08 23:46:11 +0200

    return a key with the biggest integer value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
         return None
     largest = ['', 0]
     for key, value in a_dictionary.items():
-        if value > largest[1]:
+        if value >= largest[1]:
             largest[0] = key
             largest[1] = value
     return largest[0]
