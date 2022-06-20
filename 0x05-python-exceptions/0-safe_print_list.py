b18b0991a0c38d07d42362ff50b4522bd7f409ba
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-20 22:58:25 +0200

    Print list items

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    for item in my_list:
-        if counter >= x:
+    for idx in range(x):
+        try:
+            print(my_list[idx], end="")
+        except IndexError:
             break
-        print(item, end="")
-        counter += 1
+        else:
+            counter += 1
     print()
     return counter
