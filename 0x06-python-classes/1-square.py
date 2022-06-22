f231a1c85d8c091823876a474773462c8e4b6a06
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-06-22 23:59:04 +0200

    Add docstring for init

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,4 +2,8 @@
 class Square:
     """Defines a square"""
     def __init__(self, size):
+        """ Instatiate the attributes for fields
+            Args:
+                size (int): represents the size of the square
+        """
         self.__size = size
