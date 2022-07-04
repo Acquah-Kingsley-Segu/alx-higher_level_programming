895c237d809d29a2bf4b05d56a4ded0bc89c1c2a
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-07-04 21:27:01 +0200

    return the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module that shows the members of an object """
+
+
+def lookup(obj):
+    """ Returns a list of attributes and methods of an object 
+        Arg:
+            obj (class instance): object to be used
+    """
+    return dir(obj)
