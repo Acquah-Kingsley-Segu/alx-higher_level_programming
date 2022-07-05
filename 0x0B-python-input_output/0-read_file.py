5df5d4a6e1ecef7feb21e1a1ef865b6c90536b32
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-07-05 11:55:25 +0200

    read file content and output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Read a file content """
+
+
+def read_file(filename=""):
+    """ Prints the content of a file to stdout
+        Arg:
+            filename (str): name or path of file
+    """
+    with open(filename, encoding='utf-8') as fname:
+        print(fname.read())
