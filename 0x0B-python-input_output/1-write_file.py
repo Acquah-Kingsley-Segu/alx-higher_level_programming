3b28b93b358a1f71b2a47127a1439ec6b698acdf
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-07-06 01:54:31 +0200

    add docstring to function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,11 @@
 
 
 def write_file(filename="", text=""):
+    """ Write content of text to file specified in filename
+        Args:
+            * filename (str): name or path of file
+            * text (str): content to write to file
+    """
     with open(filename, 'w', encoding='utf-8') as fname:
         written_len = fname.write(text)
     return written_len
