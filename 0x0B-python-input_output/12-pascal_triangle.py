62bef71c0a63ad5c7bf32dfe68619a50749353b0
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-07-06 03:57:04 +0200

    Preform pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module with function that perform pascal triangle """
+
+
+def pascal_triangle(n):
+    """ Perform pascal triangle from 0 to n.
+        It then returns a list of lists
+        Arg:
+            * n (int): maximum pascal term
+    """
+
+    if n <= 0:
+        return []
+    row_list = []
+    pascal = []
+    for row in range(n):
+        for col in range(row+1):
+            if col == 0 or col == row:
+                row_list.append(1)
+            else:
+                row_list.append(pascal[row-1][col-1] + pascal[row-1][col])
+        pascal.append(row_list)
+        row_list = []
+    return pascal
