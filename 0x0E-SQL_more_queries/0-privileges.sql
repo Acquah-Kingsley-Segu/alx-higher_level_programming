d0e3990fbd275bc8559c1bb080d7fd043a823cc9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-11 00:28:37 +0200

    Show grants mades available for two users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- List priviledges assigned to users: user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1';
-SHOW GRANTS FOR 'user_0d_2';
+-- List priviledges assigned to users user_0d_1 on localhost 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List priviledges assigned to user user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
