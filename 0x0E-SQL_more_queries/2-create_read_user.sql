dcb4769532b3dee9ec0f4cd6b4368c3cf198924b
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-11 14:35:55 +0200

    Fix position of the if clause

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- create a database
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create a user
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2';
 
 -- grant select permission to user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
