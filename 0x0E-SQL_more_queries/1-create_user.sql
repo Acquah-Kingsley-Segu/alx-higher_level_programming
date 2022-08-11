644fee7033ebc6eb02737c643f2dbd44e9cb0ae9
Acquah Kingsley Segu <ksegu.acq@icloud.com>
2022-08-11 14:00:53 +0200

    Create user and grant all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- create a user
+CREATE USER IF NOT EXISTS "usere_0d_1"@"localhost" IDENTIFIED WITH mysql_native_password BY "user_0d_1";
+
+-- grant permissions to user
+GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
+
+-- flush privileges
+FLUSH PRIVILEGES;
